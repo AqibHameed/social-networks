81dc6f43df671bb7c21253b65e421b2eabd44826
aqibhameed <aqibpadana@gmail.com>
2018-10-29 00:09:06 +0500

    assam and cancan gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = "Access denied."
+    redirect_to posts_url
+  end
 end
--- app/models/friend_ship.rb
+++ app/models/friend_ship.rb
@@ -1,5 +1,32 @@
 class FriendShip < ApplicationRecord
+  include AASM
 
+  aasm :column => 'state' do
+    state :incompleted, :initial => true
+    state :pending_requested
+    state :cancel_requested
+    state :confirmed
+
+    event :pending_request do
+      transitions :from => :incompleted, :to => :pending_requested
+    end
+
+    event :cancel_request do
+      transitions :from => :pending_requested, :to => :cancel_requested
+    end
+
+    event :resum_request do
+      transitions :from => :cancel_requested, :to => :pending_requested
+    end
+
+    event :confirm do
+      transitions :from => :pending_requested, :to => :confirmed
+    end
+
+    # event :unfriend do
+    #    transition :from => [:pending_requested, :confirmed], :to => :incompleted
+    # end
+  end
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 
