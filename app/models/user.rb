4064bdc57e1bf1fed235978a26f5d6b97706adc1
aqibhameed <aqibpadana@gmail.com>
2018-12-09 18:05:22 +0500

    self join

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
 
   has_many :friend_ships, dependent: :destroy
   has_many :friends, through: :friend_ships
+  has_many :inverse_friend_ships, class_name: "FriendShip", foreign_key: "friend_id"
+  has_many  :inverse_friends, through: :inverse_friend_ships, source: :user
   has_many :posts
   has_many :comments
   has_many :likes
